c2a14fc52d6b726f6ee91138ad5c17b416cac143
Teatoller <steven.otieno1@gmail.com>
2019-10-14 16:57:18 +0300

    (component): Immplement component in app
    - add contactcard component to app
    - add attributes
    
    [Starts #006]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import ContactCard from "./ContactCard";
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contacts">
+      <ContactCard
+       contacts = {{name:"Mr. Whiskerson",
+        imgUrl: "http://placekitten.com/300/200",
+        phone: "(212) 555-1234",
+        email:"mr.whiskaz@catnap.meow"}}
+      />
+
+      <ContactCard
+       contacts = {{name: "Fluffykins",
+        imgUrl: "http://placekitten.com/400/200",
+        phone: "(212) 555-2345",
+        email: "fluff@me.com"}}
+      />
+
+      <ContactCard
+        contacts= {{name:"Destroyer",
+        imgUrl: "http://placekitten.com/400/300",
+        phone: "(212) 555-3456",
+        email: "ofworlds@yahoo.com"}}
+      />
+
+      <ContactCard
+       contacts = {{name: "Felix",
+        imgUrl:"http://placekitten.com/200/100",
+        phone:"(212) 555-4567",
+        email:"thecat@hotmail.com"}}
+      />
     </div>
   );
 }
